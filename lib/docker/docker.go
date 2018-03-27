afeae20c3161e078a77113546af553815480190a
Julian Phillips <julian@quantumfyre.co.uk>
2018-03-27 23:11:19 +0100

    Refactor out code for running command in container
    As the first step towards allowing the running of arbitrary commands
    during container setup refactor the code for running a command in the
    container out of the setup function.
    
    Also, start separating the docker package into the main (i.e. public)
    parts and the utility (i.e. private) parts.
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/docker/docker.go
+++ lib/docker/docker.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -23,10 +22,8 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/versions"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/docker/pkg/term"
-	"github.com/docker/docker/registry"
 	"golang.org/x/net/context"
 )
 
@@ -115,100 +112,20 @@ func (c *Connection) setup(id string, crate *config.Crate) error {
 		cmd = append(cmd, "--extra-group", group)
 	}
 
-	config := types.ExecConfig{
-		AttachStdin:  false,
-		AttachStdout: true,
-		AttachStderr: true,
-		Tty:          false,
-		User:         "root:root",
-		Cmd:          cmd,
-	}
-
-	resp, err := c.c.ContainerExecCreate(c.ctx, id, config)
-	if err != nil {
-		return err
-	}
-
-	execID := resp.ID
-	if execID == "" {
-		return fmt.Errorf("Got empty exec ID")
-	}
-
-	startCheck := types.ExecStartCheck{
-		Tty: false,
-	}
-	attach, err := c.c.ContainerExecAttach(c.ctx, execID, startCheck)
-	if err != nil {
-		return err
-	}
-	defer attach.Close()
-
 	buf := &bytes.Buffer{}
 
-	outChan := make(chan error)
-	go func() {
-		_, err := stdcopy.StdCopy(ioutil.Discard, buf, attach.Reader)
-		log.Printf("Copy done")
-		outChan <- err
-	}()
-
-	// Wait for copies to finish
-	if err = <-outChan; err != nil {
-		return fmt.Errorf("Error copying output: %s", err)
+	exitCode, err := c.run(id, cmd, nil, nil, buf)
+	if err != nil {
+		return err
 	}
 
 	log.Printf("Setup stderr: %s", buf)
 
-	inspect, err := c.c.ContainerExecInspect(c.ctx, execID)
-	if err != nil {
-		return fmt.Errorf("Failed to get exec response: %s", err)
-	}
-
-	if inspect.Running {
-		return fmt.Errorf("Setup command still running!")
-	}
-
-	if inspect.ExitCode != 0 {
-		return fmt.Errorf("Setup command failed (%d): %s", inspect.ExitCode, buf)
+	if exitCode != 0 {
+		return fmt.Errorf("Setup command failed (%d): %s", exitCode, buf)
 	}
 
 	return nil
-}
-
-func (c *Connection) pullImage(name string) error {
-	ref, err := reference.ParseNormalizedNamed(name)
-	if err != nil {
-		return err
-	}
-
-	repoInfo, err := registry.ParseRepositoryInfo(ref)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("REF: %v REG: %v", ref, repoInfo)
-
-	auth, err := config.LoadAuth()
-	if err != nil {
-		log.Printf("Failed to load saved auth: %s", err)
-	}
-
-	options := types.ImageCreateOptions{
-		RegistryAuth: auth[repoInfo.Index.Name],
-		Platform:     "linux",
-	}
-
-	resp, err := c.c.ImageCreate(c.ctx, name, options)
-	if err != nil {
-		return err
-	}
-	defer resp.Close()
-
-	fd, term := term.GetFdInfo(os.Stderr)
-
-	log.Printf("PULL: image:%s fd:%d term:%v", name, fd, term)
-
-	return jsonmessage.DisplayJSONMessagesStream(resp, os.Stderr, fd, term, nil)
 }
 
 func (c *Connection) Create(crate *config.Crate) (string, error) {
