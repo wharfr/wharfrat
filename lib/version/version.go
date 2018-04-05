f249f1b3ea6f3f8c127e4ea8a30cc6cf184f1bd7
Julian Phillips <julian@quantumfyre.co.uk>
2018-04-05 01:55:31 +0100

    Tweak the version output
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/version/version.go
+++ lib/version/version.go
@@ -4,16 +4,17 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"runtime"
 	"time"
 
 	"git.qur.me/qur/wharf_rat/lib/docker"
 )
 
-var versionString = "eyJ2ZXJzaW9uIjoidW5rbm93biIsImdvdmVyc2lvbiI6InVua25vd24ifQo="
+var versionString = "eyJ2ZXJzaW9uIjoidW5rbm93biIsImNvbW1pdCI6InVua25vd24ifQo="
 
 type versionInfo struct {
 	Version   string
-	GoVersion string
+	Commit    string
 	BuildTime time.Time
 }
 
@@ -21,8 +22,9 @@ var version versionInfo
 
 func ShowVersion() error {
 	fmt.Printf("Version: %s\n", version.Version)
+	fmt.Printf("Git Commit: %s\n", version.Commit)
 	fmt.Printf("Docker API Version: %s\n", docker.Version())
-	fmt.Printf("Go Version: %s\n", version.GoVersion)
+	fmt.Printf("Go Version: %s\n", runtime.Version())
 	fmt.Printf("Built: %s\n", version.BuildTime.Local())
 	return nil
 }
