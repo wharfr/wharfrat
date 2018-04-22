4f09c964a322d55105ed4c929eb021ca914ffb59
Julian Phillips <julian@quantumfyre.co.uk>
2018-04-22 01:54:18 +0100

    Tweak list output
    Add an option to output the information in a JSON form, also remove and
    common path from the project paths to try and shorten the table width.
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/cmd/wharfrat/list.go
+++ lib/cmd/wharfrat/list.go
@@ -13,6 +13,7 @@ import (
 )
 
 type List struct {
+	JSON bool `short:"j" long:"json" description:"JSON output instead of table"`
 }
 
 type triState int
@@ -37,6 +38,36 @@ type listEntry struct {
 	image   string
 	state   string
 	branch  strState
+}
+
+type tree map[string]tree
+
+func (t tree) Add(path string) {
+	parts := strings.Split(path, string(os.PathSeparator))
+	if strings.HasPrefix(path, "/") {
+		parts = append([]string{"/"}, parts...)
+	}
+	t.add(parts)
+}
+
+func (t tree) add(path []string) {
+	name, rest := path[0], path[1:]
+	if _, ok := t[name]; !ok {
+		t[name] = tree{}
+	}
+	if len(rest) != 0 {
+		t[name].add(rest)
+	}
+}
+
+func (t tree) Prefix() string {
+	if len(t) != 1 {
+		return ""
+	}
+	for name, sub := range t {
+		return filepath.Join(name, sub.Prefix())
+	}
+	panic("should never reach here")
 }
 
 func (t triState) fmt() string {
@@ -92,6 +123,8 @@ func (l *List) Execute(args []string) error {
 	entries := []listEntry{}
 	maxName, maxProject, maxBranch, maxCrate, maxImage := 14, 14, 16, 5, 5
 
+	projects := tree{}
+
 	for _, container := range containers {
 		projectFile := container.Labels["me.qur.wharf-rat.project"]
 		crateName := container.Labels["me.qur.wharf-rat.crate"]
@@ -107,6 +140,8 @@ func (l *List) Execute(args []string) error {
 		if err != nil && !os.IsNotExist(err) && err != config.CrateNotFound {
 			return fmt.Errorf("Failed to lookup crate: %s", err)
 		}
+
+		projects.Add(filepath.Dir(project))
 
 		projectBranch, err := vc.Branch(project)
 		if err != nil {
@@ -151,10 +186,6 @@ func (l *List) Execute(args []string) error {
 			maxName = len(name)
 		}
 
-		if len(project) > maxProject {
-			maxProject = len(project)
-		}
-
 		if len(crateName) > maxCrate {
 			maxCrate = len(crateName)
 		}
@@ -173,31 +204,60 @@ func (l *List) Execute(args []string) error {
 		})
 	}
 
-	fmt.Printf("\033[37;1m%-*s\033[0m | ", maxName, "Container Name")
-	fmt.Printf("\033[37;1m%-*s\033[0m | ", maxProject, "Project Folder")
-	fmt.Printf("\033[37;1m%-*s\033[0m | ", maxBranch, "Container Branch")
-	fmt.Printf("\033[37;1m%-*s\033[0m | ", maxCrate, "Crate")
-	fmt.Printf("\033[37;1m%-*s\033[0m | ", maxImage, "Image")
-	fmt.Printf("\033[37;1m%s\033[0m\n", "Container State")
-	fmt.Printf("%s-+-", dashes(maxName))
-	fmt.Printf("%s-+-", dashes(maxProject))
-	fmt.Printf("%s-+-", dashes(maxBranch))
-	fmt.Printf("%s-+-", dashes(maxCrate))
-	fmt.Printf("%s-+-", dashes(maxImage))
-	fmt.Printf("%s\n", dashes(15))
-	for _, entry := range entries {
-		fmt.Printf("%-*s", maxName, entry.name)
-		fmt.Printf("\033[0m | ")
-		fmt.Printf("%s%-*s", entry.project.state.fmt(), maxProject, entry.project.str)
-		fmt.Printf("\033[0m | ")
-		fmt.Printf("%s%-*s", entry.branch.state.fmt(), maxBranch, entry.branch.str)
-		fmt.Printf("\033[0m | ")
-		fmt.Printf("%s%-*s", entry.crate.state.fmt(), maxCrate, entry.crate.str)
-		fmt.Printf("\033[0m | ")
-		fmt.Printf("%-*s", maxImage, entry.image)
-		fmt.Printf("\033[0m | ")
-		fmt.Printf("%s", entry.state)
-		fmt.Printf("\033[0m\n")
+	if l.JSON {
+		fmt.Printf("[\n")
+		for i, entry := range entries {
+			fmt.Printf("  {")
+			fmt.Printf(" \"name\": \"%s\",", entry.name)
+			fmt.Printf(" \"project\": \"%s\",", entry.project.str)
+			fmt.Printf(" \"branch\": \"%s\",", entry.branch.str)
+			fmt.Printf(" \"crate\": \"%s\",", entry.crate.str)
+			fmt.Printf(" \"image\": \"%s\",", entry.image)
+			fmt.Printf(" \"state\": \"%s\"", entry.state)
+			fmt.Printf("}")
+			if i+1 < len(entries) {
+				fmt.Printf(",")
+			}
+			fmt.Printf("\n")
+		}
+		fmt.Printf("]\n")
+	} else {
+		prefix := projects.Prefix()
+		log.Printf("PROJECT PREFIX: %s", prefix)
+		for i, entry := range entries {
+			short := strings.Replace(entry.project.str, prefix, "...", 1)
+			entries[i].project.str = short
+			if len(short) > maxProject {
+				maxProject = len(short)
+			}
+		}
+
+		fmt.Printf("\033[37;1m%-*s\033[0m | ", maxName, "Container Name")
+		fmt.Printf("\033[37;1m%-*s\033[0m | ", maxProject, "Project Folder")
+		fmt.Printf("\033[37;1m%-*s\033[0m | ", maxBranch, "Container Branch")
+		fmt.Printf("\033[37;1m%-*s\033[0m | ", maxCrate, "Crate")
+		fmt.Printf("\033[37;1m%-*s\033[0m | ", maxImage, "Image")
+		fmt.Printf("\033[37;1m%s\033[0m\n", "Container State")
+		fmt.Printf("%s-+-", dashes(maxName))
+		fmt.Printf("%s-+-", dashes(maxProject))
+		fmt.Printf("%s-+-", dashes(maxBranch))
+		fmt.Printf("%s-+-", dashes(maxCrate))
+		fmt.Printf("%s-+-", dashes(maxImage))
+		fmt.Printf("%s\n", dashes(15))
+		for _, entry := range entries {
+			fmt.Printf("%-*s", maxName, entry.name)
+			fmt.Printf("\033[0m | ")
+			fmt.Printf("%s%-*s", entry.project.state.fmt(), maxProject, entry.project.str)
+			fmt.Printf("\033[0m | ")
+			fmt.Printf("%s%-*s", entry.branch.state.fmt(), maxBranch, entry.branch.str)
+			fmt.Printf("\033[0m | ")
+			fmt.Printf("%s%-*s", entry.crate.state.fmt(), maxCrate, entry.crate.str)
+			fmt.Printf("\033[0m | ")
+			fmt.Printf("%-*s", maxImage, entry.image)
+			fmt.Printf("\033[0m | ")
+			fmt.Printf("%s", entry.state)
+			fmt.Printf("\033[0m\n")
+		}
 	}
 
 	return nil
