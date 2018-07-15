a8a3c7361b546efb8a568bf70ce332dd0f7cd648
Julian Phillips <julian@quantumfyre.co.uk>
2018-07-15 21:36:13 +0100

    Setup so symlinks to wharfrat will run commands
    Now you can create a symlink to wharfrat as a shortcut for "wr <name>" - so you
    have have a command on the host that will automatically run the same command
    inside the container.
    
    Option parsing is currently missing, but the intention is that you can set
    WHARFRAT_OPTIONS to specify command line options to be parsed as if "wr" had
    been called.
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/cmd/cmd.go
+++ lib/cmd/cmd.go
@@ -5,17 +5,20 @@ import (
 	"path/filepath"
 
 	"wharfr.at/wharfrat/lib/cmd/internal"
+	"wharfr.at/wharfrat/lib/cmd/proxy"
 	"wharfr.at/wharfrat/lib/cmd/wharfrat"
 	"wharfr.at/wharfrat/lib/cmd/wr"
 )
 
 func Main() int {
-	switch filepath.Base(os.Args[0]) {
+	switch name := filepath.Base(os.Args[0]); name {
 	case "wr":
 		return wr.Main()
 	case "wr-init":
 		return internal.Main()
-	default:
+	case "wharfrat":
 		return wharfrat.Main()
+	default:
+		return proxy.Main(name)
 	}
 }
