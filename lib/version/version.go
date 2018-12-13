8df0a591e3b6b239023319b48471159d3b0e9f64
Julian Phillips <julian@quantumfyre.co.uk>
2018-12-13 20:47:38 +0000

    Update version to v0.8.4
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/version/version.go
+++ lib/version/version.go
@@ -10,7 +10,7 @@ import (
 	"wharfr.at/wharfrat/lib/docker"
 )
 
-var versionString = "eyJ2ZXJzaW9uIjoidjAuOC4zLWRldiIsImNvbW1pdCI6IjAxNjBjNTBlMTM1YTZmMzM1MmIxNTM5NDdhNzk2MjlhY2I3OWY1ZjgiLCJidWlsZHRpbWUiOiIyMDE4LTEyLTEzVDIwOjE1OjQxWiJ9Cg=="
+var versionString = "eyJ2ZXJzaW9uIjoidjAuOC40IiwiY29tbWl0IjoiNzUxYTg4YzdjOWYzZDhhZjM3MWY0ZmZlMzZhODA1MDI5ZDllMmVmNiIsImJ1aWxkdGltZSI6IjIwMTgtMTItMTNUMjA6NDc6MzhaIn0K"
 
 type versionInfo struct {
 	Version   string
