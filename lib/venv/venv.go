582bedb7507011801cf4b9a8981e5024f83edfad
Julian Phillips <julian@quantumfyre.co.uk>
2019-09-28 13:42:11 +0100

    Rearrange venv sub-package a bit more
    venv.go is now only the public parts of the package. The random stuff is
    now moved to utils.go
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/venv/venv.go
+++ lib/venv/venv.go
@@ -1,12 +1,9 @@
 package venv
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -14,85 +11,6 @@ import (
 	"wharfr.at/wharfrat/lib/docker"
 	"wharfr.at/wharfrat/lib/version"
 )
-
-func copySelf(dest string) error {
-	return copyFile(dest, "/proc/self/exe")
-}
-
-func copyFile(dest, source string) error {
-	in, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	if err := os.Chmod(dest, 0755); err != nil {
-		os.Remove(dest)
-		return err
-	}
-
-	if _, err := io.Copy(out, in); err != nil {
-		os.Remove(dest)
-		return err
-	}
-
-	return nil
-}
-
-func ensure(path string) error {
-	binPath := filepath.Join(path, "bin")
-	if _, err := os.Stat(binPath); err == nil {
-		// bin path exists, so assume entire environment has been setup
-		return nil
-	}
-	if err := os.MkdirAll(binPath, 0755); err != nil {
-		return err
-	}
-	wr := filepath.Join(binPath, "wharfrat")
-	if err := copySelf(wr); err != nil {
-		return err
-	}
-	if err := os.Symlink(wr, filepath.Join(binPath, "wr")); err != nil {
-		return err
-	}
-	if err := os.Symlink(wr, filepath.Join(binPath, "wr-exec")); err != nil {
-		return err
-	}
-	return nil
-}
-
-func getBinaries(c *docker.Connection, id string, crate *config.Crate, user string, patterns []string) ([]string, error) {
-	search := []string{"/sbin/wr-init", "search", "-x"}
-	search = append(search, patterns...)
-	stdout, stderr, err := c.GetOutput(id, search, crate, user)
-	log.Printf("OUTPUT: %s %s %s", stdout, stderr, err)
-	if err != nil {
-		return nil, err
-	}
-	lines := bytes.Split(stdout, []byte("\n"))
-	paths := make([]string, 0, len(lines))
-	for _, line := range lines {
-		line = bytes.TrimSpace(line)
-		if len(line) > 0 {
-			paths = append(paths, string(line))
-		}
-	}
-	return paths, nil
-}
-
-func getCrateNames(project *config.Project) []string {
-	crates := make([]string, 0, len(project.Crates))
-	for name := range project.Crates {
-		crates = append(crates, name)
-	}
-	return crates
-}
 
 func Create(relPath string, crates []string, c *docker.Connection) error {
 	proj, err := config.LocateProject(".")
@@ -206,12 +124,6 @@ func DisplayInfo() {
 	fmt.Printf("Path: %s\n", state.EnvPath)
 	fmt.Printf("Project: %s\n", state.Project)
 	fmt.Printf("Crates: %s\n", strings.Join(state.Crates, ", "))
-}
-
-func getExternalCommit(tool string) (string, error) {
-	cmd := exec.Command(tool, "version", "--commit")
-	out, err := cmd.Output()
-	return string(bytes.TrimSpace(out)), err
 }
 
 func UpdateWharfrat(force bool) error {
