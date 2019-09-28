7d8288e0069b69c8df16c643ae8f48dc2aad1d84
Julian Phillips <julian@quantumfyre.co.uk>
2019-09-28 09:45:29 +0100

    Try and make venv package tidier
    Break up the venv package into separate files to provide more
    organisation. The current breakdown is to pull out the state methods
    into one file, and the copied lookPath function into another.
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

---
+++ lib/venv/exec.go
@@ -0,0 +1,46 @@
+package venv
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func findExecutable(file string) error {
+	d, err := os.Stat(file)
+	if err != nil {
+		return err
+	}
+	if m := d.Mode(); !m.IsDir() && m&0111 != 0 {
+		return nil
+	}
+	return os.ErrPermission
+}
+
+// lookPath is exec.LookPath from the standard library, except that it ignores
+// the given directory when searching PATH
+func lookPath(file, ignore string) (string, error) {
+	if strings.Contains(file, "/") {
+		err := findExecutable(file)
+		if err == nil {
+			return file, nil
+		}
+		return "", &exec.Error{file, err}
+	}
+	path := os.Getenv("PATH")
+	for _, dir := range filepath.SplitList(path) {
+		if dir == ignore {
+			continue
+		}
+		if dir == "" {
+			// Unix shell semantics: path element "" means "."
+			dir = "."
+		}
+		path := filepath.Join(dir, file)
+		if err := findExecutable(path); err == nil {
+			return path, nil
+		}
+	}
+	return "", &exec.Error{file, exec.ErrNotFound}
+}
--- lib/venv/venv.go
+++ lib/venv/venv.go
@@ -2,7 +2,6 @@ package venv
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -15,20 +14,6 @@ import (
 	"wharfr.at/wharfrat/lib/docker"
 	"wharfr.at/wharfrat/lib/version"
 )
-
-type binary struct {
-	Command []string `json:"command"`
-	Paths   []string `json:"paths"`
-	User    string   `json:"user"`
-	Workdir string   `json:"workdir"`
-}
-
-type state struct {
-	Project  string              `json:"project"`
-	Crates   []string            `json:"crates"`
-	Binaries map[string][]binary `json:"binaries"`
-	EnvPath  string              `json:"envpath"`
-}
 
 func copySelf(dest string) error {
 	return copyFile(dest, "/proc/self/exe")
@@ -101,23 +86,6 @@ func getBinaries(c *docker.Connection, id string, crate *config.Crate, user stri
 	return paths, nil
 }
 
-func (s *state) getDelta(crate string, paths []string) []string {
-	old := map[string]bool{}
-	for _, bin := range s.Binaries[crate] {
-		for _, path := range bin.Paths {
-			old[path] = true
-		}
-	}
-	delta := make([]string, 0, len(paths))
-	for _, path := range paths {
-		if !old[path] {
-			delta = append(delta, path)
-		}
-	}
-	log.Printf("DELTA: %s -> %s", paths, delta)
-	return delta
-}
-
 func getCrateNames(project *config.Project) []string {
 	crates := make([]string, 0, len(project.Crates))
 	for name := range project.Crates {
@@ -181,125 +149,6 @@ func Create(relPath string, crates []string, c *docker.Connection) error {
 	return nil
 }
 
-func loadState() (*state, error) {
-	envPath := os.Getenv("WHARFRAT_ENV")
-	if envPath == "" {
-		// no environment enabled
-		return nil, nil
-	}
-	s := state{}
-	statePath := filepath.Join(envPath, ".state.json")
-	f, err := os.Open(statePath)
-	if os.IsNotExist(err) {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	if err := json.NewDecoder(f).Decode(&s); err != nil {
-		return nil, err
-	}
-	if s.EnvPath != envPath {
-		return nil, fmt.Errorf("environment may have been moved?")
-	}
-	return &s, nil
-}
-
-func (s *state) MatchesCrate(crate *config.Crate) bool {
-	if crate.ProjectPath() != s.Project {
-		return false
-	}
-	for _, name := range s.Crates {
-		if crate.Name() == name {
-			return true
-		}
-	}
-	return false
-}
-
-func (s *state) createBinary(crate, path string) error {
-	_, name := filepath.Split(path)
-	refPath := filepath.Join(s.EnvPath, "bin", name)
-	f, err := os.Create(refPath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	if err := os.Chmod(refPath, 0755); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	if _, err := f.WriteString(fmt.Sprintf("#!%s/bin/wr-exec\n\n", s.EnvPath)); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	if _, err := f.WriteString(fmt.Sprintf("project = \"%s\"\n", s.Project)); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	if _, err := f.WriteString(fmt.Sprintf("crate = \"%s\"\n", crate)); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	if _, err := f.WriteString(fmt.Sprintf("command = [\"%s\"]\n", path)); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	if _, err := f.WriteString("auto-clean = true\n"); err != nil {
-		os.Remove(refPath)
-		return err
-	}
-	return nil
-}
-
-func (s *state) exportBinaries(crate string, cmd []string, user, workdir string, paths[]string) error {
-	log.Printf("EXPORT: %s %s", cmd, paths)
-	for _, path := range paths {
-		if err := s.createBinary(crate, path); err != nil {
-			return err
-		}
-	}
-	s.Binaries[crate] = append(s.Binaries[crate], binary{
-		Command: cmd,
-		Paths:   paths,
-		User:    user,
-		Workdir: workdir,
-	})
-	return nil
-}
-
-func (s *state) Save() error {
-	statePath := filepath.Join(s.EnvPath, ".state.json")
-	f, err := os.Create(statePath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	if err := json.NewEncoder(f).Encode(&s); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *state) Update(c *docker.Connection, id string, crate *config.Crate, user, workdir string, cmd []string) error {
-	paths, err := getBinaries(c, id, crate, user, crate.ExportBin)
-	log.Printf("OUTPUT: %s %s %s", cmd, paths, err)
-	if err != nil {
-		log.Printf("ERROR: Failed to update exported binaries: %s", err)
-		return err
-	}
-	delta := s.getDelta(crate.Name(), paths)
-	if len(delta) == 0 {
-		// Nothing changed
-		return nil
-	}
-	if err := s.exportBinaries(crate.Name(), cmd, user, workdir, delta); err != nil {
-		log.Printf("ERROR: Failed to export binaries: %s", err)
-		return err
-	}
-	return nil
-}
-
 func Update(c *docker.Connection, id string, crate *config.Crate, user, workdir string, cmd []string) {
 	if len(crate.ExportBin) == 0 {
 		// no export paths configured, so do nothing
@@ -322,55 +171,6 @@ func Update(c *docker.Connection, id string, crate *config.Crate, user, workdir 
 		log.Printf("ERROR: Failed to save state: %s", err)
 		return
 	}
-}
-
-func (s *state) Delete() {
-	if err := os.RemoveAll(s.EnvPath); err != nil {
-		log.Printf("ERROR: Failed to delete environment: %s", err)
-		return
-	}
-}
-
-func (s *state) Rebuild(c *docker.Connection, id string, crate *config.Crate) error {
-	name := crate.Name()
-	binaries, ok := s.Binaries[name]
-	if !ok {
-		// This crate doesn't have any exported binaries
-		return nil
-	}
-	s.Binaries[name] = nil
-	if err := s.Update(c, id, crate, "", "", nil); err != nil {
-		return fmt.Errorf("failed to update exported binaries: %s", err)
-	}
-	for _, bin := range binaries {
-		if err := s.restoreItem(c, id, crate, bin); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (s *state) restoreItem(c *docker.Connection, id string, crate *config.Crate, bin binary) error {
-	if len(bin.Command) == 0 {
-		// no command - no nothing to run
-		return nil
-	}
-	delta := s.getDelta(crate.Name(), bin.Paths)
-	if len(delta) == 0 {
-		return nil
-	}
-	log.Printf("RESTORE: %s %s", bin.Command, delta)
-	ret, err := c.ExecCmd(id, bin.Command, crate, bin.User, bin.Workdir)
-	if err != nil {
-		return fmt.Errorf("Failed to exec command (%s): %s", bin.Command, err)
-	}
-	if ret != 0 {
-		return fmt.Errorf("Running command (%s) failed", bin.Command)
-	}
-	if err := s.Update(c, id, crate, bin.User, bin.Workdir, bin.Command); err != nil {
-		return fmt.Errorf("Failed to update exported binaries: %s", err)
-	}
-	return nil
 }
 
 func Rebuild(c *docker.Connection, id string, crate *config.Crate) {
@@ -406,49 +206,6 @@ func DisplayInfo() {
 	fmt.Printf("Path: %s\n", state.EnvPath)
 	fmt.Printf("Project: %s\n", state.Project)
 	fmt.Printf("Crates: %s\n", strings.Join(state.Crates, ", "))
-}
-
-func findExecutable(file string) error {
-	d, err := os.Stat(file)
-	if err != nil {
-		return err
-	}
-	if m := d.Mode(); !m.IsDir() && m&0111 != 0 {
-		return nil
-	}
-	return os.ErrPermission
-}
-
-// lookPath is exec.LookPath from the standard library, except that it ignores
-// the given directory when searching PATH
-func lookPath(file, ignore string) (string, error) {
-	if strings.Contains(file, "/") {
-		err := findExecutable(file)
-		if err == nil {
-			return file, nil
-		}
-		return "", &exec.Error{file, err}
-	}
-	path := os.Getenv("PATH")
-	for _, dir := range filepath.SplitList(path) {
-		if dir == ignore {
-			continue
-		}
-		if dir == "" {
-			// Unix shell semantics: path element "" means "."
-			dir = "."
-		}
-		path := filepath.Join(dir, file)
-		if err := findExecutable(path); err == nil {
-			return path, nil
-		}
-	}
-	return "", &exec.Error{file, exec.ErrNotFound}
-}
-
-func (s *state) findExternalWharfrat() (string, error) {
-	binPath := filepath.Join(s.EnvPath, "bin")
-	return lookPath("wharfrat", binPath)
 }
 
 func getExternalCommit(tool string) (string, error) {
