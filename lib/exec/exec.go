c27d4a904a699b7211869be7b8cd0c5ca0b834c7
Julian Phillips <julian@quantumfyre.co.uk>
2022-02-12 13:14:36 +0000

    Add a env prune command to remove old scripts
    If there are old scripts in the env that no longer point at existing commands
    in the container, then running "env prune -r" will now remove them (or running
    just "env prune" will list them).
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/exec/exec.go
+++ lib/exec/exec.go
@@ -5,44 +5,25 @@ import (
 	"log"
 	"path/filepath"
 
-	"github.com/BurntSushi/toml"
 	"wharfr.at/wharfrat/lib/config"
 	"wharfr.at/wharfrat/lib/docker"
+	"wharfr.at/wharfrat/lib/exec/script"
 	"wharfr.at/wharfrat/lib/venv"
 )
 
-type ExecCfg struct {
-	Args      []string `toml:"args"`
-	Command   []string `toml:"command"`
-	Crate     string   `toml:"crate"`
-	Project   string   `toml:"project"`
-	User      string   `toml:"user"`
-	AutoClean bool     `toml:"auto-clean"`
-	path      string
-	meta      toml.MetaData
-}
+type ExecCfg script.Script
 
 func Parse(path string) (*ExecCfg, error) {
-	absPath, err := filepath.Abs(path)
+	s, err := script.Parse(path)
 	if err != nil {
 		return nil, err
 	}
-	var cfg ExecCfg
-	md, err := toml.DecodeFile(absPath, &cfg)
-	if err != nil {
-		return nil, err
-	}
-	log.Printf("Unknown config keys: %s", md.Undecoded())
-	log.Printf("ExecCfg File: %s", absPath)
-	log.Printf("ExecCfg: %#v", cfg)
-	cfg.path = absPath
-	cfg.meta = md
-	return &cfg, nil
+	return (*ExecCfg)(s), nil
 }
 
 func (e *ExecCfg) getCrate(ls config.LabelSource) (*config.Crate, error) {
 	path := e.Project
-	base := filepath.Dir(e.path)
+	base := filepath.Dir(e.Path)
 	if path == "" {
 		return config.GetCrate(base, e.Crate, ls)
 	}
@@ -69,10 +50,10 @@ func (e *ExecCfg) Execute(args []string) (int, error) {
 	}
 	cmd := e.Command
 	if len(cmd) == 0 {
-		name := filepath.Base(e.path)
+		name := filepath.Base(e.Path)
 		cmd = []string{name}
 	}
-	if e.meta.IsDefined("args") {
+	if e.Meta.IsDefined("args") {
 		args = e.Args
 	}
 	cmd = append(cmd, args...)
