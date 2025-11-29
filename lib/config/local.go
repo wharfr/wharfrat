097b238d5d3ac06427f642eb59805ca8372e1893
Julian Phillips <julian@quantumfyre.co.uk>
2025-11-29 14:44:54 +0000

    Fix use of deprecated toml function
    Also tidy up some lint warnings in the same file.
    
    Signed-off-by: Julian Phillips <julian@quantumfyre.co.uk>

--- lib/config/local.go
+++ lib/config/local.go
@@ -10,13 +10,13 @@ import (
 )
 
 type LocalSetup struct {
-	Project   string `toml:"project"`
-	Crate     string `toml:"crate"`
-	SetupPrep string `toml:"setup-prep"`
-	SetupPre  string `toml:"setup-pre"`
-	SetupPost string `toml:"setup-post"`
-	Tarballs  map[string]string
-	Env       map[string]string
+	Project   string            `toml:"project"`
+	Crate     string            `toml:"crate"`
+	SetupPrep string            `toml:"setup-prep"`
+	SetupPre  string            `toml:"setup-pre"`
+	SetupPost string            `toml:"setup-post"`
+	Tarballs  map[string]string `toml:"tarballs"`
+	Env       map[string]string `toml:"env"`
 	project   *regexp.Regexp
 	crate     *regexp.Regexp
 }
@@ -44,12 +44,15 @@ func loadLocal() {
 		return
 	}
 	defer f.Close()
-	md, err := toml.DecodeReader(f, &localConfig)
+
+	md, err := toml.NewDecoder(f).Decode(&localConfig)
 	if err != nil {
 		log.Printf("Failed to load local config: %s", err)
 		return
 	}
+
 	localConfig.path = f.Name()
+
 	log.Printf("Unknown config keys: %s", md.Undecoded())
 }
 
@@ -61,35 +64,45 @@ func Local() *LocalConfig {
 func (l *LocalConfig) Setup(crate *Crate) ([]*LocalSetup, error) {
 	projects := make([]*LocalSetup, 0, len(l.Setups))
 	project := crate.ProjectPath()
+
 	for i, setup := range l.Setups {
 		if setup.project == nil {
 			if setup.Project == "" {
 				setup.Project = ".*"
 			}
+
 			r, err := regexp.Compile(setup.Project)
 			if err != nil {
 				return nil, err
 			}
+
 			setup.project = r
 		}
+
 		if setup.crate == nil {
 			if setup.Crate == "" {
 				setup.Crate = ".*"
 			}
+
 			r, err := regexp.Compile(setup.Crate)
 			if err != nil {
 				return nil, err
 			}
+
 			setup.crate = r
 		}
+
 		if !setup.project.MatchString(project) {
 			continue
 		}
+
 		if !setup.crate.MatchString(crate.Name()) {
 			continue
 		}
+
 		projects = append(projects, &l.Setups[i])
 	}
+
 	return projects, nil
 }
 
